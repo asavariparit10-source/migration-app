12464ea850f486c9a7dc7439393f0c2b13e2eba4
asavariparit10-source <asavariparit10@gmail.com>
2026-07-28 17:21:46 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,9 @@
-variable "vpc_cidr" {}
-variable "db_username" {}
-variable "db_password" {}
+variable "aws_region" {
+  description = "AWS Region"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "VPC CIDR Block"
+  type        = string
+}
