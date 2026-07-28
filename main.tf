89024cf017522567d8203ecdb587f4abf2426c58
asavariparit10-source <asavariparit10@gmail.com>
2026-07-28 17:19:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,14 +9,9 @@ terraform {
   }
 }
 
-provider "aws" {
-  profile = "default"
-  region  = "ap-south-2"
-}
 
-########################################
 # VPC
-########################################
+
 
 resource "aws_vpc" "dr_vpc" {
   cidr_block           = "10.100.0.0/16"
@@ -28,9 +23,9 @@ resource "aws_vpc" "dr_vpc" {
   }
 }
 
-########################################
+
 # Internet Gateway
-########################################
+
 
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.dr_vpc.id
@@ -40,15 +35,14 @@ resource "aws_internet_gateway" "igw" {
   }
 }
 
-########################################
+
 # Public Subnet
-########################################
+
 
 resource "aws_subnet" "public" {
-
   vpc_id                  = aws_vpc.dr_vpc.id
   cidr_block              = "10.100.1.0/24"
-  availability_zone       = "ap-south-2a"
+  availability_zone       = "ap-south-1a"
   map_public_ip_on_launch = true
 
   tags = {
@@ -56,42 +50,39 @@ resource "aws_subnet" "public" {
   }
 }
 
-########################################
+
 # Private Subnet 1
-########################################
+
 
 resource "aws_subnet" "private1" {
-
   vpc_id            = aws_vpc.dr_vpc.id
   cidr_block        = "10.100.2.0/24"
-  availability_zone = "ap-south-2a"
+  availability_zone = "ap-south-1a"
 
   tags = {
     Name = "Private-Subnet-1"
   }
 }
 
-########################################
+
 # Private Subnet 2
-########################################
+
 
 resource "aws_subnet" "private2" {
-
   vpc_id            = aws_vpc.dr_vpc.id
   cidr_block        = "10.100.3.0/24"
-  availability_zone = "ap-south-2b"
+  availability_zone = "ap-south-1b"
 
   tags = {
     Name = "Private-Subnet-2"
   }
 }
 
-########################################
+
 # Public Route Table
-########################################
+
 
 resource "aws_route_table" "public_rt" {
-
   vpc_id = aws_vpc.dr_vpc.id
 
   route {
@@ -105,109 +96,58 @@ resource "aws_route_table" "public_rt" {
 }
 
 resource "aws_route_table_association" "public_assoc" {
-
   subnet_id      = aws_subnet.public.id
   route_table_id = aws_route_table.public_rt.id
 }
 
-########################################
+
 # Security Group
-########################################
 
-resource "aws_security_group" "postgres_sg" {
 
-  name   = "postgres-sg"
-  vpc_id = aws_vpc.dr_vpc.id
+resource "aws_security_group" "terraform_sg" {
+  name        = "terraform-demo-sg"
+  description = "Security Group for DR Infrastructure"
+  vpc_id      = aws_vpc.dr_vpc.id
 
   ingress {
-    description = "PostgreSQL"
-
-    from_port = 5432
-    to_port   = 5432
-    protocol  = "tcp"
-
-    cidr_blocks = [
-      "10.20.0.0/16"
-    ]
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["10.20.0.0/16"]
   }
 
   egress {
-
-    from_port = 0
-    to_port   = 0
-    protocol  = "-1"
-
-    cidr_blocks = [
-      "0.0.0.0/0"
-    ]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
-    Name = "Postgres-SG"
+    Name = "Terraform-SG"
   }
 }
 
-########################################
-# DB Subnet Group
-########################################
 
-resource "aws_db_subnet_group" "db_subnet" {
-
-  name = "fly91-dr-db-subnet"
-
-  subnet_ids = [
-    aws_subnet.private1.id,
-    aws_subnet.private2.id
-  ]
-
-  tags = {
-    Name = "Fly91-DB-Subnet"
-  }
-}
-
-########################################
-# PostgreSQL RDS
-########################################
-
-resource "aws_db_instance" "postgres" {
-
-  identifier = "fly91-dr-postgresql"
-
-  engine         = "postgres"
-  engine_version = "16.4"
-
-  instance_class = "db.t3.medium"
-
-  allocated_storage = 20
-  storage_type       = "gp3"
-
-  db_name  = "fly91dr"
-  username = "postgres"
-  password = "Password@12345"
-
-  db_subnet_group_name = aws_db_subnet_group.db_subnet.name
-
-  vpc_security_group_ids = [
-    aws_security_group.postgres_sg.id
-  ]
-
-  publicly_accessible = false
-
-  skip_final_snapshot = true
-
-  tags = {
-    Name = "Fly91-DR-PostgreSQL"
-  }
-}
-
-########################################
 # Outputs
-########################################
+
 
 output "vpc_id" {
   value = aws_vpc.dr_vpc.id
 }
 
-output "rds_endpoint" {
-  value = aws_db_instance.postgres.endpoint
+output "public_subnet_id" {
+  value = aws_subnet.public.id
+}
+
+output "private_subnet1_id" {
+  value = aws_subnet.private1.id
+}
+
+output "private_subnet2_id" {
+  value = aws_subnet.private2.id
+}
+
+output "security_group_id" {
+  value = aws_security_group.terraform_sg.id
 }
