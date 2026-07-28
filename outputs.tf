95146b06825d3c8db5f5929c07feb81de56cace2
asavariparit10-source <asavariparit10@gmail.com>
2026-07-28 17:24:51 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,19 @@
+output "vpc_id" {
+  value = aws_vpc.dr_vpc.id
+}
 
+output "public_subnet_id" {
+  value = aws_subnet.public.id
+}
+
+output "private_subnet1_id" {
+  value = aws_subnet.private1.id
+}
+
+output "private_subnet2_id" {
+  value = aws_subnet.private2.id
+}
+
+output "security_group_id" {
+  value = aws_security_group.terraform_sg.id
+}
