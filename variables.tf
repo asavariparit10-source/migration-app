79d0256d98c841dcc26606310bde7ce3d3aed338
asavariparit10-source <asavariparit10@gmail.com>
2026-07-28 15:34:20 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,10 +1,3 @@
-terraform {
-  required_version = ">= 1.5"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
+variable "vpc_cidr" {}
+variable "db_username" {}
+variable "db_password" {}
