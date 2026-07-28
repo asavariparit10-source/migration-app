73530a831957821470c40f9ab7c668f23a666d68
asavariparit10-source <asavariparit10@gmail.com>
2026-07-28 17:58:37 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,10 @@
-variable "aws_region" {
-  description = "AWS Region"
-  type        = string
-  default     = "ap-south-1"
-}
+terraform {
+  required_version = ">= 1.5"
 
-variable "vpc_cidr" {
-  description = "VPC CIDR Block"
-  type        = string
-  default     = "10.100.0.0/16"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
 }
