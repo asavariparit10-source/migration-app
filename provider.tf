1facfa5a8dd40dbdb4c84a15a74777b61ef02f65
asavariparit10-source <asavariparit10@gmail.com>
2026-07-29 12:44:27 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,11 +1,3 @@
-variable "aws_region" {
-  description = "AWS Region"
-  type        = string
-  default     = "ap-south-1"
-}
-
-variable "vpc_cidr" {
-  description = "VPC CIDR"
-  type        = string
-  default     = "10.100.0.0/16"
+provider "aws" {
+  region = var.aws_region
 }
