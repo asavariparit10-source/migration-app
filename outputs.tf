12dd72bd3e0282ca2b5db7a2b8c50b6d7015100f
asavariparit10-source <asavariparit10@gmail.com>
2026-07-29 11:17:57 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -6,14 +6,14 @@ output "public_subnet_id" {
   value = aws_subnet.public.id
 }
 
-output "private_subnet1_id" {
-  value = aws_subnet.private1.id
-}
-
-output "private_subnet2_id" {
-  value = aws_subnet.private2.id
-}
-
 output "security_group_id" {
   value = aws_security_group.terraform_sg.id
+}
+
+output "ec2_instance_id" {
+  value = aws_instance.dr_ec2.id
+}
+
+output "ec2_public_ip" {
+  value = aws_instance.dr_ec2.public_ip
 }
